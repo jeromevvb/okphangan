c0b9852c4f9d9acacefb0619053e35ffbe7a83cf
jeromevvb <jeromevvb@gmail.com>
2021-03-08 13:15:07 +0700

    unuse firebaseadmin

--- services/firebaseAdmin.ts
+++ services/firebaseAdmin.ts
@@ -1,26 +1,28 @@
 import firebaseAdmin from 'firebase-admin';
-import serviceAccount from '../secrets.json';
+// import serviceAccount from '../secrets.json';
 
-const params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url
-}
+// const params = {
+//   type: serviceAccount.type,
+//   projectId: serviceAccount.project_id,
+//   privateKeyId: serviceAccount.private_key_id,
+//   privateKey: serviceAccount.private_key,
+//   clientEmail: serviceAccount.client_email,
+//   clientId: serviceAccount.client_id,
+//   authUri: serviceAccount.auth_uri,
+//   tokenUri: serviceAccount.token_uri,
+//   authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+//   clientC509CertUrl: serviceAccount.client_x509_cert_url
+// }
    
 
-if(!firebaseAdmin.apps.length) {
-  firebaseAdmin.initializeApp({
-    credential:firebaseAdmin.credential.cert(params),
-    databaseURL:process.env.NEXT_PUBLIC_DATABASE_URL
-  })
-}
+
+
+// if(!firebaseAdmin.apps.length) {
+//   firebaseAdmin.initializeApp({
+//     credential:firebaseAdmin.credential.cert(params),
+//     databaseURL:process.env.NEXT_PUBLIC_DATABASE_URL
+//   })
+// }
 
 
 // export const verifyIdToken = (token:string) => {
